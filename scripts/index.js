8f2ea6c7fde7e3a1bcf528728077851625bf5d76
frontendpaul <matizbrave@gmail.com>
2020-11-23 21:27:19 +0100

    counter with intersection observer added

--- scripts/index.js
+++ scripts/index.js
@@ -15,17 +15,70 @@ window.addEventListener("scroll", function () {
         .classList.remove("active"));
 });
 
+// Dropdown menu handler
 document.querySelector(".has-dropdown > a").addEventListener('click', function () {
   this.parentElement
     .querySelector(".submenu")
     .classList.toggle("active"),
     this.classList.toggle("open");
 });
-
+// Open menu
 document.querySelector('.menutrigger').addEventListener('click', function () {
   document.querySelector('.header').classList.add('menu-open');
 })
-
+// Close menu
 document.querySelector('.close-menu').addEventListener('click', function () {
   document.querySelector('.header').classList.remove('menu-open');
 })
+
+
+
+// Counter
+import { CountUp } from './countUp.min.js';
+
+function countersHandler() {
+  const options = {
+    separator: '.',
+    decimal: ',',
+  };
+  
+  let counter1 = new CountUp('counter-1', 99, options);
+  if (!counter1.error) {
+    counter1.start();
+  } else {
+    console.error(counter1.error);
+  }
+  
+  let counter2 = new CountUp('counter-2', 1000000, options);
+  if (!counter2.error) {
+    counter2.start();
+  } else {
+    console.error(counter2.error);
+  }
+  
+  let counter3 = new CountUp('counter-3', 30, options);
+  if (!counter3.error) {
+    counter3.start();
+  } else {
+    console.error(counter3.error);
+  }
+}
+
+let options = {
+  rootMargin: '-50px',
+  threshold: 1.0
+}
+
+let callback = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      countersHandler();
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+let target = document.querySelector('.counter');
+observer.observe(target);
